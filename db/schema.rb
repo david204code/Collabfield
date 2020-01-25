6832ba33c59bb2eeefb9ff0b6650f3abed082493
David Wu <david204code@gmail.com>
2020-01-25 17:57:26 +0000

    Delete everything from the signup pagem except the form. Convert form into a bootstrap form. Add an additional name field

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2020_01_25_165613) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
